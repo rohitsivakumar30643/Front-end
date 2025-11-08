62ed502edc4a1f8c3662314aadb860e4afdcb5bf
B ROHIT SIVA KUMAR <rohitsivakumargowdo6@gmail.com>
2025-11-08 13:44:57 +0530

    Delete my-app directory

--- my-app/src/app/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import feedbackReducer from "../features/feedbackSlice";
-
-export const store = configureStore({
-  reducer: { feedback: feedbackReducer }
-});
